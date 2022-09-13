83856740b42c4e3b467c08ee5ac66accf067c184
Eduardo Lima <dualmeidalima@gmail.com>
2022-09-13 19:23:28 -0300

    Initial commit

---
+++ apps/ministry-maps/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'kingdom-apps-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'ministry-maps';
+}
