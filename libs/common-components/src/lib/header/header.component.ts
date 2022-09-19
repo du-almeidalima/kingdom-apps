930ec356a427ae61a173a9296d4e658e79c4f55b
Eduardo Lima <dualmeidalima@gmail.com>
2022-09-19 20:11:12 -0300

    feat: tweak header

--- libs/common-components/src/lib/header/header.component.ts
+++ libs/common-components/src/lib/header/header.component.ts
@@ -6,11 +6,13 @@ import { CommonModule } from '@angular/common';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   standalone: true,
-  imports: [CommonModule]
+  imports: [CommonModule],
 })
 export class HeaderComponent {
   @Input()
   public backgroundColorVar = 'hsl(0, 0%, 50%)';
+  @Input()
+  public logoBackgroundColor = 'hsl(0, 0%, 50%)';
 
   @Input()
   public initials = 'TT';
