9e2dcc6e1337b9b58f4f7f527dc31e2c9230a46b
Eduardo Lima <dualmeidalima@gmail.com>
2022-10-17 00:33:45 -0300

    feat: add send icon

--- libs/common/src/lib/components/icon/icon-type.ts
+++ libs/common/src/lib/components/icon/icon-type.ts
@@ -1,1 +1,1 @@
-export type Icons = 'task-list-lined' | 'picture-thin' | 'plus-lined' | 'x-mark-lined';
+export type Icons = 'task-list-lined' | 'picture-thin' | 'plus-lined' | 'x-mark-lined' | 'paper-plane-2';
