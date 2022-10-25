b8b0f473c384e288184933ce3c8736434dfc3189
Eduardo Lima <dualmeidalima@gmail.com>
2022-10-25 00:24:22 -0300

    refactor: clean up dead code

--- apps/ministry-maps/src/app/app.component.ts
+++ apps/ministry-maps/src/app/app.component.ts
@@ -1,23 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
-import { UserStateService } from './state/user.state.service';
-import { FirebaseAuthDatasourceService } from './core/features/auth/repositories/firebase/firebase-auth-datasource.service';
 
 @Component({
   selector: 'kingdom-apps-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {
-  constructor(
-    private readonly firebaseAuthDatasourceService: FirebaseAuthDatasourceService,
-    private readonly userState: UserStateService
-  ) {}
-
-  // ngOnInit(): void {
-  //   this.firebaseAuthDatasourceService.getUserFromAuthentication().subscribe(user => {
-  //     if (user) {
-  //       this.userState.setUser(user);
-  //     }
-  //   });
-  // }
-}
+export class AppComponent {}
