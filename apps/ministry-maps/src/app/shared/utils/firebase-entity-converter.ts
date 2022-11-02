33225969875e8ac219e3e7e1475771021c5d0e7d
Eduardo Lima <dualmeidalima@gmail.com>
2022-11-02 19:15:00 -0300

    feat: add customMapping to firebaseEntityConverterFactory and remove undefined properties

--- apps/ministry-maps/src/app/shared/utils/firebase-entity-converter.ts
+++ apps/ministry-maps/src/app/shared/utils/firebase-entity-converter.ts
@@ -1,13 +1,44 @@
 import { FirestoreDataConverter } from '@firebase/firestore';
 import { DocumentData, QueryDocumentSnapshot, SnapshotOptions } from '@angular/fire/firestore';
 
-export const firebaseEntityConverterFactory = <T extends object>(): FirestoreDataConverter<T> => {
+type CustomConverterFunction<T> = (data: T) => object;
+
+export const convertFirebaseTimestampToDateFactory = (field: string) => {
+  return (data: any) => ({
+    [field]: data[field]?.toDate(),
+  });
+};
+
+// This function ensures that no undefined property is sent to FireStore causing a runtime error;
+const removeUndefined = (obj: any) => {
+  for (const prop in obj) {
+    if (obj[prop] === undefined || obj[prop] === null) {
+      console.warn(`Property '${prop}' is undefined or null, removing it from the payload object to firestore.`);
+      delete obj[prop];
+    } else if (typeof obj[prop] === 'object') {
+      removeUndefined(obj[prop]);
+    }
+  }
+};
+
+export const firebaseEntityConverterFactory = <T extends object>(
+  customConverter?: CustomConverterFunction<T>
+): FirestoreDataConverter<T> => {
   return {
     toFirestore(modelObject: T): DocumentData {
+      removeUndefined(modelObject);
       return modelObject;
     },
     fromFirestore(snapshot: QueryDocumentSnapshot<T>, options?: SnapshotOptions): T {
       const data = snapshot.data(options);
+
+      if (customConverter) {
+        return {
+          ...data,
+          id: snapshot.id,
+          ...customConverter(data),
+        };
+      }
 
       return {
         ...data,
