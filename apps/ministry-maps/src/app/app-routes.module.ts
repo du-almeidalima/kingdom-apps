83bf51a9559a0d48efeb059c804c2e0dc92f4a46
Eduardo Lima <dualmeidalima@gmail.com>
2022-11-02 11:36:58 -0300

    feat: redirect to Home when logged

--- apps/ministry-maps/src/app/app-routes.module.ts
+++ apps/ministry-maps/src/app/app-routes.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { redirectUnauthorizedTo } from '@angular/fire/auth-guard';
 
 import { AuthGuard } from './core/services/auth.guard';
-import { Role } from '../models/enums/role';
+import { RoleEnum } from '../models/enums/role';
 
 export enum FeatureRoutesEnum {
   HOME = 'home',
@@ -22,13 +22,18 @@ const APP_ROUTES: Routes = [
     path: FeatureRoutesEnum.TERRITORIES,
     loadChildren: () => import('./features/territory/territory.module').then(m => m.TerritoryModule),
     canActivate: [AuthGuard],
-    data: { roles: [Role.ELDER, Role.ORGANIZER], authGuardPipe: redirectUnauthorizedToLogin },
+    data: { roles: [RoleEnum.ELDER, RoleEnum.ORGANIZER], authGuardPipe: redirectUnauthorizedToLogin },
   },
   {
     path: FeatureRoutesEnum.HOME,
     loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule),
     canActivate: [AuthGuard],
-    data: { roles: [Role.ELDER, Role.ORGANIZER], authGuardPipe: redirectUnauthorizedToLogin },
+    data: { roles: [RoleEnum.ELDER, RoleEnum.ORGANIZER], authGuardPipe: redirectUnauthorizedToLogin },
+  },
+  {
+    path: '',
+    redirectTo: FeatureRoutesEnum.HOME,
+    pathMatch: 'full',
   },
 ];
 
