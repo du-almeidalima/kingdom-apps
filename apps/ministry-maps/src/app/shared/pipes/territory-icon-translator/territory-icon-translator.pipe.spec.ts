a573d0bd2014c2214f5349061ee6993420629ea4
Eduardo Lima <dualmeidalima@gmail.com>
2022-11-15 12:06:18 -0300

    refactor: move territory icon to its own folder

---
+++ apps/ministry-maps/src/app/shared/pipes/territory-icon-translator/territory-icon-translator.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { TerritoryIconTranslatorPipe } from './territory-icon-translator.pipe';
+
+describe('TerritoryIconTranslatorPipe', () => {
+  it('create an instance', () => {
+    const pipe = new TerritoryIconTranslatorPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
--- apps/ministry-maps/src/app/shared/shared.module.ts
+++ apps/ministry-maps/src/app/shared/shared.module.ts
@@ -6,11 +6,12 @@ import { CommonComponentsModule } from '@kingdom-apps/common';
 
 import { HeaderComponent } from './components/header/header.component';
 import { MaterialCdkModule } from './modules/material/material-cdk.module';
-import { TerritoryIconTranslatorPipe } from './pipes/territory-icon-translator.pipe';
+import { TerritoryIconTranslatorPipe } from './pipes/territory-icon-translator/territory-icon-translator.pipe';
+import { VisitOutcomeToIconPipe } from './pipes/visit-outcome-to-icon/visit-outcome-to-icon.pipe';
 
 @NgModule({
-  declarations: [HeaderComponent, TerritoryIconTranslatorPipe],
+  declarations: [HeaderComponent, TerritoryIconTranslatorPipe, VisitOutcomeToIconPipe],
   imports: [CommonModule, CommonComponentsModule, RouterLinkWithHref, RouterLinkActive, MaterialCdkModule],
-  exports: [HeaderComponent, CommonComponentsModule, TerritoryIconTranslatorPipe],
+  exports: [HeaderComponent, CommonComponentsModule, TerritoryIconTranslatorPipe, VisitOutcomeToIconPipe],
 })
 export class SharedModule {}
