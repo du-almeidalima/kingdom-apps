ebd8523ed4c911410e4abbeb817b5342e683533a
Eduardo Lima <dualmeidalima@gmail.com>
2023-01-22 12:19:09 -0300

    feat: Add Maps Link placeholder

--- apps/ministry-maps/src/app/features/territory/components/territory-manage-dialog/territory-manage-dialog.component.ts
+++ apps/ministry-maps/src/app/features/territory/components/territory-manage-dialog/territory-manage-dialog.component.ts
@@ -1,11 +1,11 @@
 import { ChangeDetectionStrategy, Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { DIALOG_DATA, DialogRef } from '@angular/cdk/dialog';
+import { lastValueFrom, Observable } from 'rxjs';
 
 import { TerritoryRepository } from '../../../../repositories/territories.repository';
 import { Congregation } from '../../../../../models/congregation';
 import { Territory, TerritoryIcon } from '../../../../../models/territory';
-import { lastValueFrom, Observable } from 'rxjs';
 
 export type TerritoryDialogData = {
   territory?: Territory;
@@ -27,51 +27,56 @@ type TerritoryForm = {
   styleUrls: ['./territory-manage-dialog.component.scss'],
   template: `
     <lib-dialog title="{{ isEdit ? 'Editar Território' : 'Adicionar Território' }}">
-      <form [formGroup]="form" (ngSubmit)="handleSubmission()" id="territory-form">
+      <form [formGroup]='form' (ngSubmit)='handleSubmission()' id='territory-form'>
         <lib-form-field>
-          <label lib-label for="congregation-city">Cidade</label>
-          <select lib-select formControlName="city" type="text" id="congregation-city">
-            <option [value]="city" *ngFor="let city of data.cities">
+          <label lib-label for='congregation-city'>Cidade</label>
+          <select lib-select formControlName='city' id='congregation-city'>
+            <option [value]='city' *ngFor='let city of data.cities'>
               {{ city }}
             </option>
           </select>
         </lib-form-field>
-        <lib-form-field class="mt-5">
-          <label lib-label for="congregation-icon">Ícone</label>
-          <select lib-select formControlName="icon" type="text" id="congregation-icon">
-            <option [value]="territoryIcon" *ngFor="let territoryIcon of territoryIcons">
+        <lib-form-field class='mt-5'>
+          <label lib-label for='congregation-icon'>Ícone</label>
+          <select lib-select formControlName='icon' id='congregation-icon'>
+            <option [value]='territoryIcon' *ngFor='let territoryIcon of territoryIcons'>
               {{ territoryIcon | territoryIconTranslator }}
             </option>
           </select>
         </lib-form-field>
-        <lib-form-field class="mt-5">
-          <label lib-label for="congregation-address">Endereço</label>
-          <input lib-input formControlName="address" type="text" id="congregation-address" />
+        <lib-form-field class='mt-5'>
+          <label lib-label for='congregation-address'>Endereço</label>
+          <input lib-input formControlName='address' type='text' id='congregation-address' />
         </lib-form-field>
-        <lib-form-field class="mt-5">
-          <label lib-label for="note">Observação</label>
+        <lib-form-field class='mt-5'>
+          <label lib-label for='note'>Observação</label>
           <textarea
             lib-input
-            formControlName="note"
-            type="text"
-            id="note"
-            class="resize-y"
-            autocomplete="off"></textarea>
+            formControlName='note'
+            type='text'
+            id='note'
+            class='resize-y'
+            autocomplete='off'></textarea>
         </lib-form-field>
-        <lib-form-field class="mt-5">
-          <label lib-label for="congregation-maps-link">Link do Maps</label>
-          <input lib-input formControlName="mapsLink" type="text" id="congregation-maps-link" autocomplete="off" />
+        <lib-form-field class='mt-5'>
+          <label lib-label for='congregation-maps-link'>Link do Maps</label>
+          <input lib-input
+                 formControlName='mapsLink'
+                 type='text'
+                 id='congregation-maps-link'
+                 autocomplete='off'
+                 placeholder='https://goo.gl/maps/* ou https://maps.app.goo.gl/*' />
         </lib-form-field>
       </form>
       <lib-dialog-footer>
-        <div class="flex justify-end gap-4">
-          <button lib-button (click)="handleCancel()">Cancelar</button>
+        <div class='flex justify-end gap-4'>
+          <button lib-button (click)='handleCancel()'>Cancelar</button>
           <button
             lib-button
-            btnType="primary"
-            type="submit"
-            form="territory-form"
-            [disabled]="!this.form.valid || isSubmitting">
+            btnType='primary'
+            type='submit'
+            form='territory-form'
+            [disabled]='!this.form.valid || isSubmitting'>
             {{ isEdit ? 'Salvar' : 'Adicionar' }}
           </button>
         </div>
