ef1e0924827e60b9a855301057c6205f384346bc
Eduardo Lima <dualmeidalima@gmail.com>
2023-03-25 13:33:10 -0300

    feat(google-maps) extract link from pasted text

--- apps/ministry-maps/src/app/shared/utils/open-google-maps.ts
+++ apps/ministry-maps/src/app/shared/utils/open-google-maps.ts
@@ -1,15 +1,29 @@
 import { BrowserEnum, getAgentBrowser } from './user-agent';
 import { Territory } from '../../../models/territory';
 
+const extractLinkFromText = (link: string): string => {
+  const hyperLinkRegex =
+    /(?:(?:https?|ftp|file):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#/%=~_|$?!:,.]*\)|[A-Z0-9+&@#/%=~_|$])/gim;
+
+  try {
+    const matches = link?.match(hyperLinkRegex);
+
+    return matches?.pop() ?? link;
+  } catch (e) {
+    return link;
+  }
+};
+
 /**
  * This function is used to open Google Maps, it will try to open the maps app depending on the browser
  * the user is using.
  */
 const openGoogleMapsHandler = (mapsLink: string, territory: Territory) => {
   const agentBrowser = getAgentBrowser();
+  const sanitizedLink = extractLinkFromText(mapsLink);
 
   if (agentBrowser === BrowserEnum.CHROME) {
-    window.open(mapsLink, '_self');
+    window.open(sanitizedLink, '_self');
   } else if (agentBrowser === BrowserEnum.SAMSUNG) {
     const mapAddress = `${territory.address}, ${territory.city}`;
     const mapsUrl = new URL('https://www.google.com/maps/search/?api=1');
@@ -20,11 +34,11 @@ const openGoogleMapsHandler = (mapsLink: string, territory: Territory) => {
 
     window.open(
       `intent://${mapsHostAndPathname}#Intent;scheme=http;package=${mapsAndroidPackage};` +
-      `S.browser_fallback_url=${mapsLink}%3Fentry%3Ds&sa%3DX;` +
-      `S.intent_description=${territory.address};end`,
+        `S.browser_fallback_url=${mapsLink}%3Fentry%3Ds&sa%3DX;` +
+        `S.intent_description=${territory.address};end`
     );
   } else if (BrowserEnum.FIREFOX || BrowserEnum.SAFARI || BrowserEnum.UNKNOWN) {
-    window.open(mapsLink, '_blank');
+    window.open(sanitizedLink, '_blank');
   }
 };
 
