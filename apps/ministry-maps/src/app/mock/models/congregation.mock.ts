8d3507a48673337a11b1ba85a9165b0c1ba8958c
Eduardo Lima <dualmeidalima@gmail.com>
2023-04-22 09:03:16 -0300

    test: create user and congregation mocks

---
+++ apps/ministry-maps/src/app/mock/models/congregation.mock.ts
@@ -0,0 +1,8 @@
+import { Congregation } from '../../../models/congregation';
+
+export const CONGREGATION_MOCK: Congregation = {
+  id: '1',
+  name: 'Mock Congregation',
+  cities: ['City A', 'City B', 'City C', 'City D'],
+  locatedOn: 'City A'
+}
