b28c1ec2cbf0c3c95807781f91c157416ffa43db
Eduardo Lima <dualmeidalima@gmail.com>
2024-03-18 22:25:19 -0300

    fix(shared): showing profile link on header with logged out user

--- apps/ministry-maps/src/app/shared/components/header/header.component.ts
+++ apps/ministry-maps/src/app/shared/components/header/header.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { primaryGreen, white100 } from '@kingdom-apps/common-ui';
 import { FeatureRoutesEnum } from '../../../app-routes.module';
+import { UserStateService } from '../../../state/user.state.service';
 
 @Component({
   selector: 'kingdom-apps-header',
@@ -9,13 +10,15 @@ import { FeatureRoutesEnum } from '../../../app-routes.module';
     <lib-header [logoBackgroundColor]='headerLogoBackgroundColor' initials='MM' [headerLink]='FeatureRoutes.HOME'>
       <div class='header-container'>
         <p class='header-container__app-name'>Ministry Maps</p>
-        <a lib-icon-button
-           routerLink='/{{ FeatureRoutes.PROFILE }}'
-           title='Meu Perfil'
-           class='header-container__user-btn'
-        >
-          <lib-icon [fillColor]='userIconColor' icon='user-5'></lib-icon>
-        </a>
+        @if (userStateService.isLoggedIn) {
+          <a lib-icon-button
+             routerLink='/{{ FeatureRoutes.PROFILE }}'
+             title='Meu Perfil'
+             class='header-container__user-btn'
+          >
+            <lib-icon [fillColor]='userIconColor' icon='user-5'></lib-icon>
+          </a>
+        }
       </div>
     </lib-header>
   `,
@@ -24,4 +27,6 @@ export class HeaderComponent {
   public readonly FeatureRoutes = FeatureRoutesEnum;
   public readonly headerLogoBackgroundColor = primaryGreen;
   public readonly userIconColor = white100;
+
+  userStateService = inject(UserStateService)
 }
