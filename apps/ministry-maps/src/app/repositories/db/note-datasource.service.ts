a1d3cfb409e1e76dabe193f64c7d9a14a40114a1
Eduardo Lima <dualmeidalima@gmail.com>
2024-03-23 11:05:08 -0300

    chore: rollback dexie install until this feature is implemented

--- apps/ministry-maps/src/app/repositories/db/note-datasource.service.ts
+++ apps/ministry-maps/src/app/repositories/db/note-datasource.service.ts
@@ -1,32 +1,26 @@
-import { Injectable } from '@angular/core';
-import { db as iDb } from './app.db';
-import { NoteRepository } from '../note.repository';
-import { PickPartial } from '../../shared/utils/type-utils';
-import { Note } from '../../../models/note';
-import { from, of } from 'rxjs';
-
-@Injectable({
-  providedIn: 'root',
-})
-export class NoteDatasourceService implements NoteRepository {
-  private readonly db = iDb;
-
-  add(note: PickPartial<Note, 'createdAt' | 'id'>) {
-    const noteWithDate: Note = {
-      ...note,
-      createdAt: new Date(),
-    };
-
-    this.db.notes.add(noteWithDate);
-
-    return of(noteWithDate);
-  }
-
-  get(territoryId: string) {
-    return from(this.db.notes.get({ territoryId }));
-  }
-
-  reset() {
-    return from(this.db.resetNotes());
-  }
-}
+// Commeting this out while it's not used
+// @Injectable({
+//   providedIn: 'root',
+// })
+// export class NoteDatasourceService implements NoteRepository {
+//   private readonly db = iDb;
+//
+//   add(note: PickPartial<Note, 'createdAt' | 'id'>) {
+//     const noteWithDate: Note = {
+//       ...note,
+//       createdAt: new Date(),
+//     };
+//
+//     this.db.notes.add(noteWithDate);
+//
+//     return of(noteWithDate);
+//   }
+//
+//   get(territoryId: string) {
+//     return from(this.db.notes.get({ territoryId }));
+//   }
+//
+//   reset() {
+//     return from(this.db.resetNotes());
+//   }
+// }
