68cac677f83b9e47e38e622cfeea93f52a327ec7
Eduardo Lima <dualmeidalima@gmail.com>
2026-08-17 10:35:33 -0300

    fix(functions): add fallback project id for emulator environment

--- functions/ministry-maps/src/config/firebase.ts
+++ functions/ministry-maps/src/config/firebase.ts
@@ -2,9 +2,15 @@ import { initializeApp, getApps, applicationDefault } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 import { getAuth } from 'firebase-admin/auth';
 
+const projectId =
+  process.env['GCLOUD_PROJECT'] ||
+  process.env['GOOGLE_CLOUD_PROJECT'] ||
+  (process.env['FIRESTORE_EMULATOR_HOST'] ? 'du-ministry-maps' : undefined);
+
 if (getApps().length === 0) {
   initializeApp({
     credential: applicationDefault(),
+    ...(projectId ? { projectId } : {}),
   });
 }
 
